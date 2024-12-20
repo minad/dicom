22f2ed0ed190b08d45e1cc6acb143d3e423a530a
Daniel Mendler <mail@daniel-mendler.de>
2024-12-20 08:15:01 +0100

    Improve commentary

--- dicom.el
+++ dicom.el
@@ -30,9 +30,9 @@
 ;; DICOM files are typically used for medical imaging (US, CT, MRI, PET).  This
 ;; package adds the ability to view such files in Emacs.  The images and
 ;; metadata are displayed in regular Emacs buffers.  The package registers
-;; itself in `auto-mode-alist' for DICOMDIR and DICOM images (file extension
-;; *.dcm or *.ima).  Furthermore the command `dicom-open' opens DICOMDIR or
-;; DICOM image files interactively.
+;; itself in `auto-mode-alist' for DICOMDIR directory files and DICOM images
+;; (file extension *.dcm or *.ima).  Furthermore the command `dicom-open' opens
+;; DICOMDIR directory files or DICOM image files interactively.
 
 ;; The package relies on a few external programs, which are all widely available
 ;; on Linux distributions.
