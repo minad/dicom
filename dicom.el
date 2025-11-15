161d7b6b990cf608f5f71b4a6c840cfa57107d35
Daniel Mendler <mail@daniel-mendler.de>
2025-11-15 19:13:51 +0100

    Version 1.2

--- dicom.el
+++ dicom.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2024
-;; Version: 1.1
+;; Version: 1.2
 ;; Package-Requires: ((emacs "29.1") (compat "30"))
 ;; URL: https://github.com/minad/dicom
 ;; Keywords: multimedia, hypermedia, files
